e745ed92ab54a71e1cd5de6d78226f7963aa3a5c
Ohgyuchan <gyu3637@gmail.com>
2022-01-02 16:45:41 +0900

    add assets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+          color: Colors.red,
+          child: Column(
+            children: [Container(child: Image.asset('/assets/study1.jpg'))],
+          )),
+    );
   }
 }
