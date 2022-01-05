539c59ca9716f2af21b5625aa8443ac90ced8974
Ohgyuchan <gyu3637@gmail.com>
2022-01-05 16:45:41 +0900

    update controller

---
+++ lib/controllers/cycling_controller.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+import 'package:get/get.dart';
+
+class CyclingController extends GetxController {
+  List<String> images = [
+    'assets/study1.jpg',
+    'assets/study2.jpg',
+    'assets/study3.jpg'
+  ];
+  RxInt arg = 0.obs;
+
+  @override
+  void onInit() {
+    arg.value = Random().nextInt(3);
+    super.onInit();
+  }
+
+  void cycle() {
+    arg.value = arg.value % 3 + 1;
+    print(arg.value);
+    update();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ramdom_image/controllers/cycling_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(CyclingController());
     return Scaffold(
       body: Column(
         children: [
-          Image.asset('assets/study1.jpg'),
+          Image.asset(controller.images[controller.arg.value]),
           const SizedBox(
             height: 10,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              controller.cycle();
+            },
             icon: const Icon(Icons.change_circle),
           )
         ],
